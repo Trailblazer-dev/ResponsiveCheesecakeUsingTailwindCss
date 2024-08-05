7835038789e9e7dc824c0e7399881063934edc92
Trailblazer-dev <richvictor830@gmail.com>
2024-08-05 22:09:49 +0300

    building

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "first-color":"#523728",
+        "bg-card":"#FFF8E1",
+        "body-bg":"#FFF8E1"
+      }
+    },
+  },
+  plugins: [],
+}
+
