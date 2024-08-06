c6a54485d0dbbadcad781ffa934c78d39a5c8027
Trailblazer-dev <richvictor830@gmail.com>
2024-08-06 11:18:06 +0300

    building

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,20 @@
 
 const menu = document.getElementById('menu');
 const close = document.getElementById('close');
+const navMenu= document.getElementById('nav-menu');
 menu.addEventListener('click',()=>{
-    const navMenu= document.getElementById('nav-menu');
+    
     navMenu.classList.toggle('hidden')
 })
-
+// close nav-menu
 close.addEventListener('click',()=>{
     const navMenu= document.getElementById('nav-menu');
     navMenu.classList.add('hidden')
+});
+
+const navLInk = document.querySelectorAll('.link')
+navLInk.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.add('hidden')
+    })
 });
